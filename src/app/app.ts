09430d42ce47ecbb2c1767d24b27f18edd5671f0
AhmedMB02 <ahmedmbarek16@gmail.com>
2025-11-07 15:18:20 +0100

    ajout des card et a propos et contact

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { Footer } from "./footer/footer";
+import AOS from 'aos';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('biblio');
+export class App implements OnInit{
+  //protected readonly title = signal('biblio');
+  ngOnInit() {
+    AOS.init({
+      duration: 800, // durée anim
+      easing: 'ease-in-out', // style
+      once: true, // animé 1 seule fois
+    });
+  }
 }
---
+++ src/app/header/header.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-header',
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+
+  toggleTheme() {
+    document.body.classList.toggle('dark-theme');
+  }
+
+}
