20400db8711eda704b4cb30099c9e83f19024d4e
AhmedMB02 <ahmedmbarek16@gmail.com>
2025-11-09 23:41:40 +0100

    service et model

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {Home} from './home/home';
 import { Contact } from './contact/contact';
+import { LivreP } from './livre-p/livre-p';
 
 export const routes: Routes = [
     {path : '',component:Home},
     {path : 'contact',component:Contact},
+    {path : 'livres' , component:LivreP}
 ];
