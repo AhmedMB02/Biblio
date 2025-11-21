155f1551224c7a3056e21ee15ef1b60017b00038
AhmedMB02 <ahmedmbarek16@gmail.com>
2025-11-22 00:07:08 +0100

    some changes

--- src/app/livre-card/livre-card.ts
+++ src/app/livre-card/livre-card.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { Livre } from '../model/livre.model';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-livre-card',
-  imports: [],
+  imports: [RouterLink],
   standalone:true,
   templateUrl: './livre-card.html',
   styleUrl: './livre-card.css',
