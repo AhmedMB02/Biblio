8b48dcdb06dbb2fd454b678f69455cdcc5ab8293
AhmedMB02 <ahmedmbarek16@gmail.com>
2025-12-03 22:28:40 +0100

    verif form in contact

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,11 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
 export class Contact {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      nom: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      sujet: ['', Validators.required],
+      message: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Form Submitted!', this.contactForm.value);
+      alert('Message envoyé avec succès !');
+      this.contactForm.reset();
+    } else {
+      this.contactForm.markAllAsTouched();
+    }
+  }
 }
